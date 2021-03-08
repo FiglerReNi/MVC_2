7681993f5f28455aca0a5feafbf3dd177d1eef30
figlerr <figler.renata@tmx.hu>
2021-03-08 15:43:32 +0100

    forras

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -61,6 +61,51 @@ class Posts extends Controller
 
     }
 
+    public function edit($id){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'title' => trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'user_id' => $_SESSION['user_id'],
+                'title_err' => '',
+                'body_err' => ''
+            ];
+
+            if(empty($data['title'])){
+                $data['title_err'] = 'Please enter title';
+            }
+
+            if(empty($data['body'])){
+                $data['body_err'] = 'Please enter body text';
+            }
+
+            if(empty($data['title_err']) && empty($data['body_err'])){
+                if($this->postModel->addPost($data)){
+                    doFlash('post_message', 'Post Added');
+                    redirect('posts');
+                }else{
+                    die('Something went wrong');
+                }
+            }else{
+                $this->loadView('posts/edit', $data);
+            }
+        }else{
+            $post = $this->postModel->getPostById($id);
+            if($post->user_id != $_SESSION['user_id']){
+                redirect('posts');
+            }
+            $data = [
+                'id' => $id,
+                'title' => $post->title,
+                'body' => $post->body
+            ];
+            $this->loadView('posts/edit', $data);
+        }
+
+    }
+
     public function show($id){
         $post = $this->postModel->getPostById($id);
         $user = $this->userModel->findUserById($post['user_id']);
